c7716856f562ff33791cc9ebd5a7aa736b2f9e70
Qwerty4Uiop <alexpat930@gmail.com>
2017-12-03 17:13:43 +0300

    Fixed fitness

--- task1/GA.py
+++ task1/GA.py
@@ -9,28 +9,26 @@ carrying = items[0][0]
 capacity = items[0][1]
 del items[0]
 ga = pyeasyga.GeneticAlgorithm(items)
-sumWeight = 0
-sumVolume = 0
+sum_weight = 0
+sum_volume = 0
 
 
 def fitness(individual, data):
     fitness = 0
-    global sumWeight
-    global sumVolume
-    sumWeight = 0
-    sumVolume = 0
+    s_weight = 0
+    s_volume = 0
     for (selected, [weight, volume, profit]) in zip(individual, data):
         if selected:
-            if sumWeight + weight > carrying or sumVolume + volume > capacity:
+            if s_weight + weight > carrying or s_volume + volume > capacity:
                 fitness = 0
                 break
-            sumWeight += weight
-            sumVolume += volume
+            s_weight += weight
+            s_volume += volume
             fitness += profit
     return fitness
 
 
-def itemsDecode(codes):
+def items_decode(codes):
     result = []
     for (i, selected) in zip(range(1, len(codes)), codes):
         if selected:
@@ -38,13 +36,25 @@ def itemsDecode(codes):
     return result
 
 
+def calculate_dimensions(individual, data):
+    global sum_weight
+    global sum_volume
+    sum_weight = 0
+    sum_volume = 0
+    for (selected, [weight, volume, _]) in zip(individual, data):
+        if selected:
+            sum_weight += weight
+            sum_volume += volume
+
+
 ga.fitness_function = fitness
 ga.run()
-bestIndividual = ga.best_individual()
-print "value: " + str(int(bestIndividual[0]))
-print "weight " + str(int(sumWeight))
-print "volume " + str(int(sumVolume))
-print "items: " + str(itemsDecode(bestIndividual[1]))
+best_individual = ga.best_individual()
+calculate_dimensions(best_individual[1], items)
+print "value: " + str(int(best_individual[0]))
+print "weight " + str(int(sum_weight))
+print "volume " + str(int(sum_volume))
+print "items: " + str(items_decode(best_individual[1]))
 
 resultFile = open("result.txt", "w")
-resultFile.write(json.dumps({"value": int(bestIndividual[0]), "weight": int(sumWeight), "volume": int(sumVolume), "items": itemsDecode(bestIndividual[1])}))
+resultFile.write(json.dumps({"value": int(best_individual[0]), "weight": int(sum_weight), "volume": int(sum_volume), "items": items_decode(best_individual[1])}))
